ea163e5b25f5d9ed07b12a1187cae78a697d4720
Angus <agyn.bolatov.1992@gmail.com>
2025-04-11 22:10:09 +0500

    lesson 14

--- test/FundMeTest.sol
+++ test/FundMeTest.sol
@@ -9,10 +9,14 @@ contract FundMeTest is Test {
     uint256 favNumber = 0;
     bool greatCourse = false;
     FundMe fundMe;
+    address alice = makeAddr("alice");
+    uint256 SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(alice, STARTING_BALANCE);
     }
 
     // function testDemo() public {
@@ -30,5 +34,17 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWIthoutEnoughETH() public {
+        vm.expectRevert(); // <- The next line after this one should revert! If not test fails.
+        fundMe.fund(); // <- We send 0 value
+    }
+
+    function testFundUpdatesFundDataStructure() public {
+        vm.prank(alice);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(alice);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
